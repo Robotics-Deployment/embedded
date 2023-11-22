161ab69ce3bbc124c76d75a70ba9f14961560666
deniz <deniz.hofmeister@gmail.com>
2023-11-22 11:15:31 +0100

    Rename based on clippy

--- src/errors.rs
+++ src/errors.rs
@@ -2,24 +2,24 @@ use std::error::Error;
 use std::fmt::{self, Display};
 
 #[derive(Debug)]
-pub enum ValidationError {
-    UuidNotSet,
-    FleetNotSet,
-    InterfaceNotSet,
-    ApiUrlNotSet,
-    FileNotSet,
+pub enum ValidationNotSetError {
+    Uuid,
+    Fleet,
+    Interface,
+    ApiUrl,
+    File,
 }
 
-impl Display for ValidationError {
+impl Display for ValidationNotSetError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            ValidationError::UuidNotSet => write!(f, "UUID is not set"),
-            ValidationError::FleetNotSet => write!(f, "Fleet is not set"),
-            ValidationError::InterfaceNotSet => write!(f, "Interface is not set"),
-            ValidationError::ApiUrlNotSet => write!(f, "API URL is not set"),
-            ValidationError::FileNotSet => write!(f, "File is not set"),
+            ValidationNotSetError::Uuid => write!(f, "UUID is not set"),
+            ValidationNotSetError::Fleet => write!(f, "Fleet is not set"),
+            ValidationNotSetError::Interface => write!(f, "Interface is not set"),
+            ValidationNotSetError::ApiUrl => write!(f, "API URL is not set"),
+            ValidationNotSetError::File => write!(f, "File is not set"),
         }
     }
 }
 
-impl Error for ValidationError {}
+impl Error for ValidationNotSetError {}
